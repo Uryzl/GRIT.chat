b2f9beb2f9032a00275b10c266a4259e568c7b1c
Uryzl <jossiebatisso@gmail.com>
2024-10-14 16:26:01 -0400

    Updated code with API endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
-const express = require("express");
+require("dotenv").config();
+
 const cors = require("cors");
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +10,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {username: username, secret: username, first_name: username},
+      {headers: { "Private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY}}
+    );
+    // Status and data within the API call
+    return res.status(r.status).json(r.data);
+  } catch (error) {
+    return res.status(e.response.status).json(e.response.data)
+  }
 });
 
 app.listen(3001);
